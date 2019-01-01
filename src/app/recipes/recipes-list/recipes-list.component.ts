4f286b040b8ab3b247fa47240fa7a41f75c5ce56
iamsurajdc <surajchandgude0304@gmail.com>
2019-01-01 14:05:55 +0530

    Added recipes and shopping list navigation AND also added recipes on Select events.

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test Recipe', 'This is a test Recipe', 'https://bit.ly/2BwRBOE'),
     new Recipe('A Test-2 Recipe', 'This is a Second Recipe', 'https://bit.ly/2S9sF56')
@@ -16,6 +17,10 @@ export class RecipesListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+     this.recipeWasSelected.emit(recipe);
   }
 
 }
